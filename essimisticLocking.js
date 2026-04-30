b30550530fcd4d5344a1c644c1d8325c1cfd984b
Amit Yadav <yadavamit1173@gmail.com>
2026-04-30 21:30:17 +0530

    passimistic locking code in node.js

---
+++ essimisticLocking.js
@@ -0,0 +1,35 @@
+await client.query('BEGIN');
+
+const product = await client.query(
+  'SELECT stock FROM products WHERE id = $1 FOR UPDATE',
+  [productId]
+);
+
+if (product.rows[0].stock < quantity) {
+  throw new Error('Out of stock');
+}
+
+await client.query(
+  'UPDATE products SET stock = stock - $1 WHERE id = $2',
+  [quantity, productId]
+);
+
+await client.query('COMMIT');
+
+/*
+database sql 
+
+BEGIN;
+
+SELECT stock
+FROM products
+WHERE id = 101
+FOR UPDATE;
+
+UPDATE products
+SET stock = stock - 1
+WHERE id = 101;
+
+COMMIT;
+
+*/
